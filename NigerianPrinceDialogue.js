8ef9d53d82388280def3c6d0f71228bd14bae216
Jonathan Lynn <jonathancslynn@gmail.com>
2015-04-29 16:57:04 -0400

    more dialogue

---
+++ NigerianPrinceDialogue.js
@@ -0,0 +1,25 @@
+var n1 = "Bank Rep";
+var l1 = "Sometimes random people will send emails to you asking for money.";
+
+var n2 = "Bank Rep";
+var l2 = "They promise to reward you if you pay them a small fee.";
+
+var n3 = "Bank Rep";
+var l3 = "Others may lie and say your family has an emergency and you have to send in money.";
+
+var n4 = "Bank Rep";
+var l4 = "These are email scams. Don't fall for them.";
+
+var n5 = "Bank Rep";
+var l5 = "A common example is the Nigerian Prince scam.";
+
+var n6 = "Bank Rep";
+var l6 = "Always verify the identity of the sender.";
+
+var n7 = "Bank Rep";
+var l7 = "Don't send money to people you don't know, even if it seems legitimate.";
+
+var n8 = "Bank Rep";
+var l8 = "And if you get emails like these, report them.";
+
+dial = [[n1,l1],[n2,l2],[n3,l3],[n4,l4],[n5,l5],[n6,l6],[n7,l7],[n8,l8]];
--- RichPersonBankDialogue.js
+++ RichPersonBankDialogue.js
@@ -1,73 +1,73 @@
 var n1 = "Bank Rep";
 var l1 = "Good morning. How may I help you?";
 
-var n2 = "Player";
-var l2 = "I want to set up a bank account.";
+var n2 = "Agent";
+var l2 = "I want to set up a bank account for my player.";
 
 var n3 = "Bank Rep";
 var l3 = "Okay. I will open up a checking and savings account.";
 
-var n4 = "Player";
+var n4 = "Agent";
 var l4 = "What's a checking and savings account?";
 
 var n5 = "Bank Rep";
 var l5 = "A checking account helps keep your money safe.";
 
-var n6 = "Player";
+var n6 = "Agent";
 var l6 = "Okay.";
 
 var n7 = "Bank Rep";
 var l7 = "You can easily access your money for daily spendings.";
 
-var n8 = "Player";
+var n8 = "Agent";
 var l8 = "How?";
 
 var n9 = "Bank Rep";
 var l9 = "With a debit card or checks, that we provide.";
 
-var n10 = "Player";
+var n10 = "Agent";
 var l10 = "What about a savings account?";
 
 var n11 = "Bank Rep";
 var l11 = "Saving accounts save your money for the future";
 
-var n12 = "Player";
+var n12 = "Agent";
 var l12 = "Okay...";
 
 var n13 = "Bank Rep";
 var l13 = "They reward you with interest for the money you save.";
 
-var n14 = "Player";
+var n14 = "Agent";
 var l14 = "I see.";
 
 var n15 = "Bank Rep";
 var l15 = "Shall we get started?";
 
-var n16 = "Player";
+var n16 = "Agent";
 var l16 = "Yes!";
 
 var n17 = "Bank Rep";
 var l17 = "Okay, first I need your opening deposit.";
 
-var n18 = "Player";
+var n18 = "Agent";
 var l18 = "Here it is.";
 
 var n19 = "Bank Rep";
 var l19 = "Thanks. Are you aware of our services and fees?";
 
-var n20 = "Player";
-var l20 = "Yes, my agent went through everything with me.";
+var n20 = "Agent";
+var l20 = "Yes, I've done my research.";
 
 var n21 = "Bank Rep";
 var l21 = "Perfect. Let me process everything right now.";
 
-var n22 = "Player";
+var n22 = "Agent";
 var l22 = "Awesome!";
 
 var n23 = "Bank Rep";
 var l23 = "Congratulations, your account has been created.";
 
-var n24 = "Player";
+var n24 = "Agent";
 var l24 = "Thank you so much!";
 
 var dial=[[n1,l1],[n2,l2],[n3,l3],[n4,l4],[n5,l5],[n6,l6],[n7,l7],[n8,l8],[n9,l9],[n10,l10],[n11,l11],[n12,l12],[n13,l13],[n14,l14],[n15,l15],[n16,l16],[n17,l17],[n18,l18],[n19,l19],[n20,l20],[n21,l21],[n22,l22],[n23,l23],[n24,l24]];
