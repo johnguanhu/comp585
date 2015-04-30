478be6dd8df203dbfb9b5d8510f1b1feb29253f9
Jonathan Lynn <jonathancslynn@gmail.com>
2015-04-29 21:07:25 -0400

    more dialogue

---
+++ IntroDialogue.js
@@ -0,0 +1,29 @@
+var n1 = "Computer";
+var l1 = "Welcome to planet earth, agent. ";
+
+var n2 = "Computer";
+var l2 = "A rising basketball star has come to our city.";
+
+var n3 = "Computer";
+var l3 = "He has chosen our sports agency, and we need you to be his agent.";
+
+var n4 = "Computer";
+var l4 = "You will be tasked to manage his finances ...";
+
+var n5 = "Computer";
+var l5 = "... and help him get settled in our city.";
+
+var n6 = "Computer";
+var l6 = "You need to help him make good financial decisions, ...";
+
+var n7 = "Computer";
+var l7 = "... and at the same time keep him happy.";
+
+var n8 = "Computer";
+var l8 = "Our company's success is in your hands.";
+
+var n9 = "Computer";
+var l9 = "Good luck, and don't let us down.";
+
+
+dial = [[n1,l1],[n2,l2],[n3,l3],[n4,l4],[n5,l5],[n6,l6],[n7,l7],[n8,l8],[n9,l9]];
---
+++ InvestmentTipsDialogue.js
@@ -0,0 +1,17 @@
+var n1 = "Bank Rep";
+var l1 = "As you make more money, you should consider ways to invest it.";
+
+var n2 = "Bank Rep";
+var l2 = "There are many ways you can capitalize on your money.";
+
+var n3 = "Bank Rep";
+var l3 = "However, not all investments are good ones.";
+
+var n4 = "Bank Rep";
+var l4 = "Invest in ventures that promise good return.";
+
+var n5 = "Bank Rep";
+var l5 = "Do your research. Don't invest blindly.";
+
+
+dial = [[n1,l1],[n2,l2],[n3,l3],[n4,l4],[n5,l5]];
